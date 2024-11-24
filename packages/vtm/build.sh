3cd7f4b258a2d1b1786c788bbd71aa0659876128
HanzoDev1375 <moderpostmanjava@gmail.com>
2024-11-24 20:23:12 +0330

    sub

---
+++ packages/vtm/build.sh
@@ -0,0 +1,16 @@
+TERMUX_PKG_HOMEPAGE=https://vtm.netxs.online/
+TERMUX_PKG_DESCRIPTION="Terminal multiplexer with TUI window manager and multi-party session sharing"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_LICENSE_FILE="LICENSE"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="0.9.99.50"
+TERMUX_PKG_SRCURL=https://github.com/netxs-group/vtm/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=efdc1b2c6091065e2b9271e251acda63c6fec3bfd2b99995de7192f51d090879
+TERMUX_PKG_DEPENDS="libc++"
+TERMUX_PKG_BUILD_DEPENDS="libandroid-spawn"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_pre_configure() {
+	CXXFLAGS+=" -pthread"
+}
